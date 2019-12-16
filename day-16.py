ba6709de1c0d6534a9ab5c41effc6e092fedc3ac
Christopher Lorton <bitbucket@lortons.net>
2019-12-16 02:29:52 -0800

    Day 16.

--- day-16.py
+++ day-16.py
@@ -32,35 +32,51 @@ def do_phase(_inputs, count, show=True, verbose=False):
     return ''.join([f'{_}' for _ in current])
 
 
+def part2(inputs, phases=100, verbose=False):
+
+    offset = int(''.join([f"{inputs[index]}" for index in range(7)]))
+    tenk = 10000 * len(inputs)
+
+    data = np.zeros((phases+1, tenk-offset), dtype=np.uint8)
+    for index in range(offset, tenk):
+        data[0, index-offset] = inputs[index % len(inputs)]
+
+    # print(f"{data[0, 0:8]}")
+    for phase in range(phases):
+        total = 0
+        for index in range(tenk-1, offset-1, -1):
+            total += data[phase, index-offset]
+            total %= 10
+            data[phase+1, index-offset] = total
+        if verbose:
+            print(f"{phase+1:03}: {data[phase+1, 0:8]}")
+
+    return f"{data[phases, 0:8]}"
+
+
 def main():
 
     inputs = "59765216634952147735419588186168416807782379738264316903583191841332176615408501571822799985693486107923593120590306960233536388988005024546603711148197317530759761108192873368036493650979511670847453153419517502952341650871529652340965572616173116797325184487863348469473923502602634441664981644497228824291038379070674902022830063886132391030654984448597653164862228739130676400263409084489497532639289817792086185750575438406913771907404006452592544814929272796192646846314361074786728172308710864379023028807580948201199540396460310280533771566824603456581043215999473424395046570134221182852363891114374810263887875638355730605895695123598637121"
     inputs = [int(digit) for digit in list(inputs)]
 
+    # Test case 1
     do_phase([int(digit) for digit in list("12345678")], 4, show=True)  # , verbose=True)
     print()
+    # Test cases 2, 3, 4
     print(do_phase([int(digit) for digit in list("80871224585914546619083218645595")], 100, False))
     print(do_phase([int(digit) for digit in list("19617804207202209144916044189917")], 100, False))
     print(do_phase([int(digit) for digit in list("69317163492948606335995924319873")], 100, False))
+    # Actual input
     print(do_phase(inputs, 100, False))
 
     # Part 2
 
-    seven = 5976521
-    tenk = len(inputs) * 10000
-
-    data = np.zeros((101, tenk-seven), dtype=np.uint8)
-    for index in range(seven, tenk):
-        data[0, index-seven] = inputs[index % len(inputs)]
-
-    print(f"000: {data[0, 0:8]}")
-    for phase in range(100):
-        total = 0
-        for index in range(tenk-1, seven-1, -1):
-            total += data[phase, index-seven]
-            total %= 10
-            data[phase+1, index-seven] = total
-        print(f"{phase+1:03}: {data[phase+1, 0:8]}")
+    # Test cases 1, 2, 3
+    print(part2([int(digit) for digit in "03036732577212944063491565474664"]))
+    print(part2([int(digit) for digit in "02935109699940807407585447034323"]))
+    print(part2([int(digit) for digit in "03081770884921959731165446850517"]))
+    # Actual input
+    print(part2(inputs))
 
     return
 
